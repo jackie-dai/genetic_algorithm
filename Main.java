643bcc0ac3d849c17fffa5fc85cade00fe82cac9
kutekirisaki <jackie123768@gmail.com>
2022-04-28 03:40:30 -0700

    fix main structure

--- Main.java
+++ Main.java
@@ -3,65 +3,81 @@ import java.util.HashMap;
 
 class Main {
   public static void main(String[] args) {
-
-    String[] population = setup();
-    double[] populationFitness = fitness(population);
-
-    ArrayList<String> mostFit = newPop(population, populationFitness);
-
-    ArrayList<String> newPop = crossover(mostFit);
-    System.out.println(population);
-    System.out.println(newPop);
-
     /*
-    for(int i=0; i<populationFitness.length;i++) {
-      System.out.println(populationFitness[i]);
+    Flag = false
+    Setup 
+    While(Flag != true) {
+      Check
+      Select
+      Crossover
+      Mutate
     }
-    HashMap<Integer, Integer> map = new HashMap(5);
-    map.put(2,4);
-    System.out.println(map.get(2));
     */
+    ArrayList<String> population = setup();
+    boolean flag = true;
+    int gen = 1;
+  
+    while (gen != 10) {
+      for(int i=0;i<population.size();i++) {
+        if (population.get(i).equals(Global.getTarget())) {
+          flag = true;
+        } 
+      }
+
+      population = fitness(population);
+      System.out.println(population);
+      population = crossover(population);
+      System.out.println("Generation: " + gen);
+      System.out.println(population);
+      gen++;
+    }
+  
+
+    //fitness(population);
+
   }
 
-  public static String[] setup() {
+  public static ArrayList<String> setup() {
     String alphabet = "abcdefghijklmnopqrstuvwxyz ";
-    String[] population = new String[Global.getPopulationSize()];
+    ArrayList<String> pop = new ArrayList<>();
+    String newWord;
     int index;
     
     for(int i=0;i<Global.getPopulationSize();i++) {
-        population[i] = "";
+      newWord = "";
       for(int j=0;j<Global.getTarget().length();j++) {
         index = (int) (Math.random() * 27);
-        population[i] += alphabet.substring(index,index+1);
+        newWord += alphabet.substring(index,index+1);
       }
+      pop.add(newWord);
     }
-    return population;
+    return pop;
   }
 
-  public static double[] fitness(String[] population) {
+  public static ArrayList<String> fitness(ArrayList<String> population) {
     double fit;
-    double[] arr = new double[population.length];
+    double[] arr = new double[population.size()];
 
-    for(int i=0;i<population.length;i++) {
+    for(int i=0;i<population.size();i++) {
       fit = 0.0;
-      for(int j=0;j<population[i].length();j++) {
-      if (population[i].substring(j,j+1).equals(Global.getTarget().substring(j,j+1))) {
+      for(int j=0;j<population.get(i).length();j++) {
+        if (population.get(i).substring(j,j+1).equals(Global.getTarget().substring(j,j+1))) {
         fit += (double) 1/Global.getTarget().length() * 100;
-      }
+        }
       arr[i] = fit;
+      }
     }
+    return newPop(population, arr);
   }
-    return arr;
-}
 
-  public static ArrayList<String> newPop(String[] population, double[] populationFitness) {
+  public static ArrayList<String> newPop(ArrayList<String> population, double[] populationFitness) {
     ArrayList<String> newPop = new ArrayList<String>();
     double probablity;
 
-    for(int i=0;i<population.length;i++) {
+    for(int i=0;i<population.size();i++) {
       probablity = Math.random() * 100;
       if(probablity < populationFitness[i]) {
-        newPop.add(population[i]);
+        newPop.add(population.get(i));
       }
     }
     return newPop;
@@ -74,6 +90,7 @@ class Main {
     String newStr = "";
 
     for(int i=0;i<mostFit.size()/2; i++) {
+      newStr = "";
       str1 = mostFit.remove(0);
       str2 = mostFit.remove(0);
       for(int j=0; j<str1.length();j++) {
@@ -83,7 +100,7 @@ class Main {
           newStr += str2.substring(j,j+1);
         }
       }
-     newPop.add(newStr);
+      newPop.add(newStr);
     }
     if(newPop.isEmpty() != true) {
       newPop.add(mostFit.remove(0));
